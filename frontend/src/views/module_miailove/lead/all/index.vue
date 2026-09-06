84f4eac6f14f9407e27c580e01fb94135101f649
wc1098 <30488564+wc1098@users.noreply.github.com>
2026-09-06 11:54:31 +0800

    Delete frontend directory

--- frontend/src/views/module_miailove/lead/all/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <LeadPoolPage view="all" title="全量线索" />
-</template>
-
-<script setup lang="ts">
-import LeadPoolPage from "../components/LeadPoolPage.vue";
-</script>
--- frontend/src/views/module_miailove/lead/store-pool/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <LeadPoolPage view="storePool" title="门店公海" />
-</template>
-
-<script setup lang="ts">
-import LeadPoolPage from "../components/LeadPoolPage.vue";
-</script>
